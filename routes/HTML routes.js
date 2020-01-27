7ba9ffb3eee0ae27b5f006d3bf592de93d2be574
Ben Honken <honkenb@gmail.com>
2020-01-27 14:48:14 -0600

    removed unecessary packages, updated template to match actual need.

--- routes/HTML routes.js
+++ routes/HTML routes.js
@@ -1,75 +1,18 @@
 var path = require("path");
-var db = require("../models");
-var passport = require("../config/passport");
-var isAuthenticated = require("../config/isAuthenticated");
 
 module.exports = function(app) {
     // Load index page
     app.get("/", function(req, res) {
 
-        res.sendFile(path.join(__dirname, "../views/index.html"));
+        res.sendFile(path.join(__dirname, "../index.html"));
 
     });
-    app.get("/store", function(req, res) {
+    app.get("/exercise", function(req, res) {
 
-        res.sendFile(path.join(__dirname, "../views/store.html"));
+        res.sendFile(path.join(__dirname, "../exercise.html"));
     });
+    app.get("/stats", function(req, res) {
 
-    app.get("/profile", function(req, res) {
-        if (db.Users.role === "student") {
-            res.redirect("/student");
-        } else if (db.Users.role === "tutor") {
-            res.redirect("/tutor");
-        } else if (db.Users.role === "admin") {
-            res.redirect("/admin");
-        }
-        else{
-            res.redirect("/")
-        }
-
-
+        res.sendFile(path.join(__dirname, "../stats.html"));
     });
-
-    app.get("/student", function(req, res) {
-        try{
-            if (req.user.role === "student") {
-                res.sendFile(path.join(__dirname, "../views/student.html"));
-            }
-            else{
-                res.redirect("/")
-            }
-        }
-        catch{
-            res.redirect("/")
-        }
-    });
-
-    app.get("/admin", function(req, res) {
-        try{
-            if (req.user.role === "admin") {
-                res.sendFile(path.join(__dirname, "../views/admin.html"));
-            }
-            else{
-                res.redirect("/")
-            }
-        }
-        catch{
-            res.redirect("/")
-        }
-    });
-
-    app.get("/tutor", function(req, res) {
-        try{
-            if (req.user.role === "tutor") {
-                res.sendFile(path.join(__dirname, "../views/tutor.html"));
-            }
-            else{
-                res.redirect("/")
-            }
-        }
-        catch{
-            res.redirect("/")
-        }
-    });
-
 };
