7ef2b144a2ec85ac83ff0be82e898e8e1bbfacc4
Ben Honken <honkenb@gmail.com>
2020-01-27 15:22:53 -0600

    updated server to remove unnecessary package and add a listener

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // Requiring necessary npm packages
 var express = require("express");
-var session = require("express-session");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
@@ -14,18 +13,16 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-// We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Use compression
 app.use(compression());
 
 // Requiring our routes
-require("./routes/htmlRoutes.js")(app);
-require("./routes/apiRoutes.js")(app);
+require("./routes/HTML routes.js")(app);
+require("./routes/API routes.js")(app);
 
-
+app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+});
 var mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
